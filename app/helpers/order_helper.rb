eb295ccfb19de215763ceb09f8eb94a9f24b1fff
Ian Mortier <ian.mortier@shopify.com>
2012-12-05 17:42:13 -0600

    Match the app landing page to admin 2 orders

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -1,5 +1,46 @@
 module OrderHelper
   def order_status(order)
-    content_tag :span, order.financial_status, :class => "label success o-status o-#{order.financial_status}"
+    case order.financial_status
+    when 'authorized'
+      c = 'attention'
+      i = '<i class="ico ico-16 ico-auth-expiry"></i>'
+    when 'pending'
+      c = 'pending'
+      i = ''
+    when 'success'
+      c = 'success'
+      i = ''
+    when 'complete'
+      c = 'complete'
+      i = ''
+    when 'warning'
+      c = 'warning'
+      i = ''
+    when 'error'
+      c = 'error'
+      i = ''
+    else
+      c = ''
+      i = ''
+    end
+    content_tag :span, "#{i} #{order.financial_status}", :class => "label success #{c}"
+  end
+  
+  def fulfillment_status(order)
+    case order.fulfillment_status
+      when 'partial'
+        s = 'partial'
+        c = 'attention'
+      when 'warning'
+        s = 'warning'
+        c = 'warning'
+      when 'shipped'
+        s = 'fulfilled'
+        c = 'fulfilled'
+      else
+        s = 'not fulfilled'
+        c = 'warning'
+    end
+    content_tag :span, "#{s}", :class => "label #{c}"
   end
 end
