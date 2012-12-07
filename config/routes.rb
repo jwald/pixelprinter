fd6f892109b0a8c83ee061b869eae6cc7b93767e
Christopher Lobay <hello@christopherlobay.com>
2012-12-07 11:58:18 -0600

    Removed unneeded routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :orders, :only => [:index, :show], :member => {:preview => :get, :print => :post}
   map.resources :print_templates, :as => 'templates'
   
-  map.resources :print_templates, :only => [:index, :show, :edit]
-  
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
