3b83d834467cbe18e3736544746251e9f95c9b24
Christopher Lobay <hello@christopherlobay.com>
2012-12-13 13:57:25 -0600

    Modified JS files to work with old RJS stuff

--- public/javascripts/adf/framework.js
+++ public/javascripts/adf/framework.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
 
   // Dropdowns
-  $(document).on('click', '[data-dropdown]', function(e){
+  $('[data-dropdown]').live('click', function(e){
     var selector = '.' + $(this).attr('class') + ' ' + $(this).attr('data-dropdown');
     $(selector).toggleClass('is-visible');
     e.preventDefault();
     e.stopPropagation();
   });
-  $(document).on('click', function(){
+  $(document).live('click', function(){
     $('.dropdown.is-visible').removeClass('is-visible');
   });
-  $(document).on('click', '.dropdown', function(e){
+  $('.dropdown').live('click', function(e){
     e.stopPropagation();
   });
   
   // Modals
-  $(document).on('click', '[data-modal]', function(e){
+  $('[data-modal]').live('click', function(e){
     var selector = $(this).attr('data-modal');
     $(selector).removeClass('hidden').addClass('visible');
     e.preventDefault();
     e.stopPropagation();
   });
-  $(document).on('click', '.close-modal, .modal-bg', function(){
+  $('.close-modal, .modal-bg').live('click', function(){
     $('.modal-bg').removeClass('visible').addClass('hidden');
   });
   
@@ -32,45 +32,22 @@ $(document).ready(function(){
       $('#global-notification').removeClass('is-visible');
     }, 3000);
   }
-  $(document).on('click', '.close-notification', function(){
+  $('.close-notification').live('click', function(){
     $('#global-notification').removeClass('is-visible');
   });
   
   // Tooltips
-  $(document).on('mouseenter', '[data-tooltip]',  function(){
+  $('[data-tooltip]').bind('mouseenter', function(){
     var width = $(this).outerWidth(),
         text = $(this).attr('data-tooltip');
     $(this).addClass('tooltip is-active').append('<span class="container"><span class="label">' + text + '</span></span>');
-  }).on('mouseleave', '[data-tooltip]', function() {
+  }).bind('mouseleave', function() {
     $(this).removeClass('is-active');
   });
   
   // Dirty forms
-  $(document).on('change', '[data-formchange]', function() {
+  $('[data-formchange]').bind('change keyup', function() {
     var selector = $(this).attr('data-formchange');
     $(selector).addClass('primary');
-  });
-  
-  // Select All Tables
-  $(document).on('change', 'thead input[type="checkbox"]', function(e) {
-    var parent = $(this).parents('table');
-    if($(this).prop("checked")) {
-      parent.find('input[type="checkbox"]').prop({ checked : true });
-    } else {
-      parent.find('input[type="checkbox"]').prop({ checked : false });
-    }
-    e.preventDefault();
-  });
-  $(document).on('change', 'tbody input[type="checkbox"]', function(e) {
-    var parent = $(this).parents('table'),
-        checkall = parent.find('thead input[type="checkbox"]');
-    if(checkall.length) {
-      if(checkall.prop("checked")) {
-        checkall.prop({ checked : false });
-      } else {
-        checkall.prop({ checked : false });
-      }
-    }
-    e.preventDefault();
   });
 });
