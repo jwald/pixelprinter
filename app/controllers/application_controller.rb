1ec233429cac33c5761d82df946058c3d0438f7a
Alex Coco <alex.coco@shopify.com>
2013-07-10 15:48:11 -0400

    Added Order Printer modal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  before_filter :find_app_shop
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
@@ -11,7 +12,7 @@ class ApplicationController < ActionController::Base
   protected
   
   def shop
-    Shop.find_or_create_by_url(current_shop.url)
+    Shop.find_or_create_by_url(current_shop.try(:url))
   end
 
   # Show 404 page with better error explanation when an Order or Template can't be found
@@ -23,7 +24,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # Maintenance screen - remove for production, as it will block all requests !!!
-  # before_filter { render :file => "#{RAILS_ROOT}/public/maintenance.html" }
-
+  def find_app_shop
+    @app_shop ||= shop
+  end
 end
