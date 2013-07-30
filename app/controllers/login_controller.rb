4e2bd300a37af62192d04a9ae5cd3ccdc94c6f68
Christopher Lobay <hello@christopherlobay.com>
2013-07-30 11:15:08 -0500

    Improved flash error message

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,5 +1,5 @@
 class LoginController < ApplicationController
-  
+
   def index
     # don't ask user for his #{shop}.myshopify.com address if it is already provided
     redirect_to :controller => 'login', :action => "authenticate", :shop => params[:shop] if params[:shop].present?
@@ -22,14 +22,14 @@ class LoginController < ApplicationController
 
   # Shopify redirects the logged-in user back to this action along with
   # the authorization token t.
-  # 
+  #
   # This token is later combined with the developer's shared secret to form
   # the password used to call API methods.
   def finalize
     shopify_session = ShopifyAPI::Session.new(params[:shop], params[:t])
     if shopify_session.valid?
       session[:shopify] = shopify_session
-      
+
       if Shop.find_by_url(shopify_session.url)
         session.delete :new_user
         flash[:notice] = "Successfully logged into shopify store."
@@ -42,16 +42,16 @@ class LoginController < ApplicationController
       redirect_to :action => 'index'
     end
   end
-  
+
   def logout
     session[:shopify] = nil
     flash[:notice] = "Successfully logged out."
-    
+
     redirect_to :action => 'index'
   end
-  
+
   private
-  
+
   def myshopify_url(url)
     if url =~ /\w+\.myshopify\.com\z/
       url
@@ -59,10 +59,10 @@ class LoginController < ApplicationController
       "#{url}.myshopify.com"
     end
   end
-  
+
   def block_new_shop
-    flash[:error] = "Could not create PixelPrinter account."
+    flash[:error] = "PixelPrinter has been deprecated. Please <a href='http://apps.shopify.com/order-printer'>install Order Printer</a> instead."
     session[:new_user] = true
     redirect_to :action => 'index'
   end
-end 
+end
